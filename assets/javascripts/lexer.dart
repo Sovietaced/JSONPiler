3b83854d6db7a631c7e2bde26da9a9923156a589
Sovietaced <Sovietaced@gmail.com>
2014-01-30 00:47:13 -0800

    strip source code

--- assets/javascripts/lexer.dart
+++ assets/javascripts/lexer.dart
@@ -3,7 +3,11 @@ import 'dart:html';
 
     void lex(MouseEvent event)
     {   
-      TextAreaElement source = querySelector("#input-code");
-      print(source.value);
+      var source = (querySelector("#input-code") as TextAreaElement).value;
+      // Trim the leading and trailing spaces.
+      source = source.trim();
+      // Strip all whitespace
+      source = source.replaceAll(new RegExp(r'\s+'), '\n');
+      print(source);
     }
 
