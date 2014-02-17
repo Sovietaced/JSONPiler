b0479346aab653b419efca4049fe87d0bf20da81
Sovietaced <Sovietaced@gmail.com>
2014-02-16 23:13:22 -0800

    starting to make sense of how to start parsing, beginning with statements

--- web/compiler/parser.dart
+++ web/compiler/parser.dart
@@ -1,7 +1,9 @@
-/* parser.dart  
+/* parser.dart
+ * Jason Parraga <Sovietaced@gmail.com>  
  * 
  * Implements recursive descent parsing to analyze tokens
  * */
+
 import 'token.dart';
 import 'package:logging/logging.dart';
 
@@ -15,30 +17,67 @@ class Parser{
   
   Parser(this.tokens);
   
+  /* This is the main method for the Parser where all the magic happens */
   analyse(){
+    log.info("Parser analyzing...");
     if(!tokens.isEmpty){
-      
-      while(index < tokens.length){
-        Token token = tokens[index];
-        print("parting");
-        index++;
-      }
+       if(tokens.first.type == TokenType.OPEN_BRACE){
+         index++;
+         statement();
+       }
+       else{
+         log.error("Program does not start with a block");
+         // quit
+       }
     }
     else{
       log.warning("No tokens to parse, finished.");
     }
   }
   
-  getNextToken(){
-    return this.tokens[index+1];
+  Token getNextToken(){
+    if(index < this.tokens.length - 1){
+      return this.tokens[index+1];
+    }
+    else{
+      return null;
+    }
   }
   
-  expect(TokenType token){
+  bool expect(TokenType token){
     Token next = getNextToken();  
     if(next != token){
       log.severe("Unexpected symbol " + next.toString() + ", expected " + token.toString());
       return false;
     }
     return true;
+  }
+  
+  /* Determines if the next token is the type of token
+   * that we're looking for. Used for determing the next statement. 
+   */
+  bool isNextToken(TokenType token){
+    Token next = getNextToken();  
+    if(next != token){
+      return false;
+    }
+    return true;
+  }
+  
+  void block(){
+    Token next = getNextToken();
+  }
+  
+  void varDeclaration(){
+    
+  }
+  
+  void statement(){
+    Token token = getNextToken();
+    
+    if(token.type == TokenType.PRINT){
+      print("We have a print token");
+    }
+    
   }
 }
